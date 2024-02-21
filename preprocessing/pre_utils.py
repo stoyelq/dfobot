90a9be9f67c1b4512187ebd33fe63d7bc1717e6d
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2024-02-21 11:20:54 -0400

    preprocessing pipeline

--- preprocessing/pre_utils.py
+++ preprocessing/pre_utils.py
@@ -7,68 +7,136 @@ import torch
 import cv2
 
 DATA_DIR = "/home/stoyelq/Documents/dfobot_data/preprocessing/"
+CROP_DIR = "/home/stoyelq/Documents/dfobot_data/cropped_singles/"
+TEST_DIR = "/home/stoyelq/Documents/dfobot_data/cropped_singles_test/"
+AUGMENTED_DIR = "/home/stoyelq/Documents/dfobot_data/augmented/"
+
+BUFFER_PX = 5
+AREA_THRESHOLD = 0.5
+OUT_DIM = (512, 512)
+TEST_TRAIN_SPLIT = 0.90
+
+def crop_and_save(img, contour, out_dir, buffer=5, outdim=(256, 256)):
+    rect = cv2.boundingRect(contour)  # x, y, w, h
+    x1 = rect[0] - buffer
+    y1 = rect[1] - buffer
+    x2 = (rect[0] + rect[2]) + buffer
+    y2 = (rect[1] + rect[3]) + buffer
+    cropped = img[y1:y2, x1:x2]
+    try:
+        scaled = cv2.resize(cropped, dsize=outdim)
+        saved = cv2.imwrite(out_dir, scaled)
+        if not saved:
+            print("Could not save {out_dir}".format(out_dir=out_dir))
+    except cv2.Error as e:
+        print("Error {e}. Could not save {out_dir}".format(e=e, out_dir=out_dir))
+
+def crop_and_isolate():
+    # load images
+    img_list = os.listdir(DATA_DIR)
+    count = len(img_list)
+    for img_name in img_list:
+        count += -1
+        if count == 652:
+            # that otolith touches edge of image and breaks things, ignore : )
+            print(img_name)
+            continue
+        if count % 100 == 0:
+            print(count)
+        if count < TEST_TRAIN_SPLIT * len(img_list):
+            out_dir = CROP_DIR
+        else:
+            out_dir = TEST_DIR
+        img_path = DATA_DIR + img_name
+        img = cv2.imread(img_path)
+        if img is None:
+            print(f"file {img_path} could not be read, check with os.path.exists()")
+            continue
+        # assert img is not None, f"file {img_path} could not be read, check with os.path.exists()"
+
+        # clip on threshold and convert to grayscale:
+        ret, thresh = cv2.threshold(img, 40, 255, 0)
+        imgray = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+
+        # Find contours and sort using contour area
+        cnts = cv2.findContours(imgray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+        # always save largest otolith/contour:
+        crop_and_save(img, cnts[0], out_dir=f"{out_dir}otolith_1_{img_name}", buffer=BUFFER_PX, outdim=OUT_DIM)
+        cv2.drawContours(img, [cnts[0]], -1, (36, 255, 12), 3)
+
+        # grab second otolith if area is closish:
+        first_area = cv2.contourArea(cnts[0])
+        second_area = cv2.contourArea(cnts[1])
+        if second_area > AREA_THRESHOLD * first_area:
+            crop_and_save(img, cnts[1], out_dir=f"{out_dir}otolith_2_{img_name}", buffer=BUFFER_PX, outdim=OUT_DIM)
+
+
+        # visualization tool:
+        # cv2.drawContours(img, [cnts[1]], -1, (36,255,12), 3)
+        # cv2.imshow('image', img)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
 
 def load_dmapps_report():
     gt_file = os.path.join("/home/stoyelq/Documents/dfobot_data/GT_metadata.csv")
     gt_data = pd.read_csv(gt_file)
     return gt_data
-def img_paths():
-    return os.listdir(DATA_DIR)
-
-def split_image(img_path):
-    import cv2
-    import numpy as np
-    import os
-    # Read image
-    DATA_DIR = "/home/stoyelq/Documents/dfobot_data/preprocessing/"
-    img_list = os.listdir(DATA_DIR)
-    img_path = DATA_DIR + img_list[0]
-
-    img = cv2.imread(img_path)
-    assert img is not None, "file could not be read, check with os.path.exists()"
-    ret, thresh = cv2.threshold(img, 40, 255, 0)
-    imgray = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
-    # Find contours and sort using contour area
-
-    cnts = cv2.findContours(imgray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-    for c in cnts[:2]:
-        # Highlight largest contour
-        cv2.drawContours(img, [c], -1, (36, 255, 12), 3)
-
-    cv2.imshow('thresh', thresh)
-    cv2.imshow('image', img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
 
 
-def process_image(img_path):
-    img = Image.open(img_path)
-
+def process_image(img):
     preprocess = transforms.Compose([
         transforms.ToTensor(),
         v2.ToDtype(torch.uint8, scale=True),
         v2.Grayscale(1),
-        transforms.Resize(512),
-        transforms.CenterCrop(488),
         v2.ToDtype(torch.float32, scale=True),
-        transforms.Normalize(
-            mean=[0.406],
-            std=[0.225]
-        )
     ])
 
     img_out = preprocess(img)
-    print(img_out.shape)
     return img_out
 
-def process_and_augment_image(processed_img):
-    img = processed_img
+def process_and_augment_image(img):
     preprocess = transforms.Compose([
+        transforms.ToTensor(),
+        v2.ToDtype(torch.uint8, scale=True),
         v2.RandomRotation(180),
+        v2.Grayscale(1),
+        v2.ToDtype(torch.float32, scale=True),
     ])
 
     img_out = preprocess(img)
-    print(img_out.shape)
     return img_out
+
+
+def process_and_augment_images():
+    img_list = os.listdir(CROP_DIR)
+    count = len(img_list)
+    for img_name in img_list:
+        count += -1
+        if count % 100 == 0:
+            print(count)
+        fish_id = img_name.split("photo")[0][-5:-1]
+        # make file names unique based on photo, otolith number
+        oto_count = int(img_name.split("otolith")[1][1]) if "otolith" in img_name else 1
+        photo_count = int(img_name.split("photo")[1][1]) if "photo" in img_name else 1
+        fish_count = oto_count + 2 * photo_count - 2
+
+        img_path = CROP_DIR + img_name
+        img = Image.open(img_path)
+        image_tensor = process_image(img)
+        torch.save(image_tensor, f"{AUGMENTED_DIR}{fish_id}__{fish_count}.pt")
+        augmented_img_1 = process_and_augment_image(img)
+        augmented_img_2 = process_and_augment_image(img)
+        torch.save(augmented_img_1, f"{AUGMENTED_DIR}{fish_id}__{fish_count}_augmented_1.pt")
+        torch.save(augmented_img_2, f"{AUGMENTED_DIR}{fish_id}__{fish_count}_augmented_2.pt")
+
+        # visulizers:
+        # plt.imshow(augmented_img_1.permute(1, 2, 0))
+        # plt.show()
+        # plt.imshow(augmented_img_2.permute(1, 2, 0))
+        # plt.show()
+        # plt.imshow(image_tensor.permute(1, 2, 0))
+        # plt.show()
+
